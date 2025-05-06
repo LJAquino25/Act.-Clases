2b4a41e958a19992a2f37dd1f0094e706ebc83d1
LJAquino25 <00036725@uca.edu.sv>
2025-05-06 10:35:10 -0600

    update :memo:

--- 6_Clase/clase6.cpp
+++ 6_Clase/clase6.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int SALARY,OPTION,ISSS,AFP,RENT;
+    float SALARY,ISSS,AFP,RENT,NETSALARY;
+    char OPTION;
     cout<<"Enter your current salary: ";
     cin >> SALARY;
     cout <<"-----Menu-----" <<endl;
@@ -14,28 +15,46 @@ int main(){
 
     switch(OPTION){
         case 1:
-            cout <<"" <<endl;
-            cin >>;
-            
-            cout <<": " << <<endl;
+            cout <<"Your salary: " <<SALARY <<endl;
+            ISSS = SALARY * 0.3;
+            cout <<"Your discount: " << ISSS <<endl;
+            if (ISSS <= 30){
+                cout <<"if the discount applies"
+            }else{
+                cout <<"no sicount applies"
+            }
             break;
         case 2:
-            cout <<"" <<endl;
-            cin >>;
-            
-            cout <<": " << <<endl;
+            cout <<"Your salary" <<SALARY <<endl;
+            AFP = SALARY * 0.0725;
+            cout <<"Your discount: " <<AFP <<endl;
             break;
         case 3:
-            cout <<"" <<endl;
-            cin >>;
-            
-            cout <<" " << <<endl;
+            cout <<"Your salary: " <<SALARY <<endl;
+            cout <<"Net salary: " <<NETSALARY <<endl;
+            ISSS = SALARY * 0.3;
+            AFP = SALARY * 0.0725;
+            NETSALARY = ISSS - AFP;
+            if(NETSALARY < 472.99){
+                cout <<"no discount applies" <<endl;
+            }else if (NETSALARY >= 473 && NETSALARY <=895.24 ){
+                cout <<"Discount of 10%: " <<RENT <<endl;
+                RENT = NETSALARY*0.1
+            }else if (NETSALARY >= 895.25 && NETSALARY <= 2038.10){
+                cout <<"Discount of 20%: " <<RENT <<endl;
+                RENT = NETSALARY*0.2
+            }else if (NETSALARY > 2039){
+                cout <<"Discount of 30%: "<<RENT <<endl;
+                RENT = NETSALARY*0.3
+            }
             break;
         case 4:
-            cout <<"current salary: ";
-            cout <<"discount: ";
-            cout <<"ISSS: ";
-            cout <<"AFP: ";
+            cout <<"current salary: " <<SALARY <<endl;
+            cout <<"Total discount: ";
+            cout <<"ISSS: " <<ISSS <<endl;
+            ISSS = SALARY * 0.3;
+            cout <<"AFP: " <<AFP << endl;
+            AFP = SALARY * 0.0725;
             cout <<"Rent: ";
             cout <<"Total: ";
             break;
