db5968e54c4d8e0eed34169cd1296ab02888c5d1
LJAquino25 <00036725@uca.edu.sv>
2025-06-03 09:55:37 -0600

    update

--- 12_Clase/Clase12.cpp
+++ 12_Clase/Clase12.cpp
@@ -3,15 +3,21 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int tarifa;
+    int precio, tarifa;
     cout<<"Ingrese su tarifa: ";
     cin >>tarifa;
     if (tarifa<100){
-        cout<<"Tarifa baja";
+        cout<<"Tarifa baja"<<endl;
+        precio = tarifa *0.184249; 
+        cout <<"Factura: $" <<precio;
     }else if(tarifa >=100 && tarifa <=200){
-        cout <<"Tarifa media";
+        cout <<"Tarifa media"<<endl;
+        precio = tarifa *0.183496 ; 
+        cout <<"Factura: $" <<precio;
     }else{
-        cout<<"tarifa alta";
+        cout<<"tarifa alta"<<endl;
+        precio = tarifa *0.182482; 
+        cout <<"Factura: $" <<precio;
     }
     return 0;
 }
